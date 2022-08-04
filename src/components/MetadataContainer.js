d8cc708c2e5855e23208d43058a063210d247845
alizs10 <ali.text77@gmail.com>
2022-08-04 04:50:35 +0430

    update: modal

--- src/components/MetadataContainer.js
+++ src/components/MetadataContainer.js
@@ -1,8 +1,9 @@
-import { Box, Button, Modal, Typography } from '@mui/material'
+import { Box, Button, Grid, ListItem, Modal, TextField, Typography } from '@mui/material'
 import React, { useContext, useState } from 'react'
 import MusicContext from './context/MusicContext'
 import Metadata from './Metadata'
-
+import CloudDownloadOutlinedIcon from '@mui/icons-material/CloudDownloadOutlined';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 export default function MetadataContainer() {
 
   const { title, album, artist, genre, track, year } = useContext(MusicContext)
@@ -12,11 +13,11 @@ export default function MetadataContainer() {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 800,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
     boxShadow: 24,
     p: 4,
+    borderRadius: '5px'
   };
   return (
     <div>
@@ -36,12 +37,76 @@ export default function MetadataContainer() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid item xs={12}>
+              <ListItem>Change Tags</ListItem>
+            </Grid>
+            <Grid item xs={6}>
+              <ListItem>
+                <TextField fullWidth id="outlined-basic" label="Title" variant="outlined" value={title} />
+              </ListItem>
+            </Grid>
+            <Grid item xs={6}>
+              <ListItem>
+                <TextField fullWidth id="outlined-basic" label="Album" variant="outlined" value={album} />
+              </ListItem>
+            </Grid>
+            <Grid item xs={6}>
+              <ListItem>
+                <TextField fullWidth id="outlined-basic" label="Artist" variant="outlined" value={artist} />
+              </ListItem>
+            </Grid>
+            <Grid item xs={6}>
+              <ListItem>
+                <TextField fullWidth id="outlined-basic" label="Track" variant="outlined" value={track} />
+              </ListItem>
+            </Grid>
+            <Grid item xs={6}>
+              <ListItem>
+                <TextField fullWidth id="outlined-basic" label="Year" variant="outlined" value={year} />
+              </ListItem>
+            </Grid>
+            <Grid item xs={6}>
+              <ListItem>
+                <Button
+                  fullWidth
+                  variant="outlined"
+                  component="label"
+                  style={{padding:"14px"}}
+                >
+                  <ImageOutlinedIcon style={{marginRight: "1rem"}}/>
+                  Upload Cover Art
+                  <input
+                    type="file"
+                    hidden
+                  />
+                </Button>
+
+              </ListItem>
+            </Grid
+            >
+            <Grid item xs={12}>
+              <ListItem>
+                <Button
+                  fullWidth
+                  variant="outlined"
+                  component="label"
+                  color='success'
+                  style={{padding:"14px"}}
+                >
+                  <CloudDownloadOutlinedIcon style={{marginRight: "1rem"}}/>
+                  Save And Download
+                  <input
+                    type="file"
+                    hidden
+                  />
+                </Button>
+
+              </ListItem>
+            </Grid>
+
+
+          </Grid>
         </Box>
       </Modal> : null}
 
